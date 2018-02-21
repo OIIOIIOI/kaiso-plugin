1ba921c5c94dd0dcf5a76cbad8b04f38ba2494f8
unknown <sylvere.armange@gmail.com>
2018-02-21 23:41:51 +0100

    cpt

--- kaiso-plugin.php
+++ kaiso-plugin.php
@@ -9,3 +9,18 @@ Version:      1.0
 Author:       01101101
 Text Domain:  krptd
 */
+
+function register_post_types ()
+{
+  register_post_type('distributor',
+    array(
+      'labels' => array(
+        'name' => __( 'Distributeurs' ),
+        'singular_name' => __( 'Distributeur' )
+      ),
+      'public' => true,
+      'has_archive' => false,
+    )
+  );
+}
+add_action('init', 'register_post_types');
